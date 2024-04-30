e3e9420ade99f9a2f12455b06d6c9f9751296d0e
Leesarah721 <162943735+Leesarah721@users.noreply.github.com>
2024-04-30 11:20:56 +0900

    회원가입, 로그인, 라우터 추가

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import morgan from "morgan";
-import tweetsRouter from './router/tweets.js'
+import tweetsRouter from './router/tweets.js';
+import authRouter from './router/auth.js';
 
 const app = express();
-app.use(express.json());  // json로 연결
 
+app.use(express.json());
 app.use(morgan("dev"));
+
 app.use('/tweets', tweetsRouter);  //tweetsRouter 미들웨어 등록
+app.use('/auth', authRouter); //authRouter 미들웨어 등록
+
 app.use((req, res, next) => {
     res.sendStatus(404);
 });
--- router/tweets.js
+++ router/tweets.js
@@ -1,40 +1,50 @@
-            import express from "express";
-            import * as tweetController from '../controller/tweet.js'
-
-            const router = express.Router();
-
-            // 해당 아이디에 대한 트윗 가져오기
-            // GET
-            // http://localhost:8080/tweets?username=:username
-            router.get('/', tweetController.getTweets);
+import express from "express";
+import * as tweetController from "../controller/tweet.js" ;
+import { body } from 'express-validator'; //추가
+import { validate } from "../middlewear/validator.js"; //추가
 
 
-            // 글번호에 대한 트윗 가져오기
-            // GET
-            // http://localhost:8080/tweets/:id
-            router.get('/:id', tweetController.getTweet);
+const router = express.Router();
+
+/* 
+post, put에 text에 대해 빈문자열을 없애고, 최소 3자 이상 입력해야 데이터를 저장하도록 API에 적용
+*/
+const validateTweet = [
+    body('text').trim().isLength({min: 3}).withMessage('최소 3자 이상 입력'), validate
+] //추가
+
+// 해당 아이디에 대한 트윗 가져오기
+// GET
+// http://localhost:8080/tweets?username=:username
+router.get('/', tweetController.getTweets);
 
 
-            // 트윗하기
-            // POST
-            // http://localhost:8080/tweets
-            // name, username, text
-            // json 형태로 입력 후 추가된 데이터까지 모두 json으로 출력
-            router.post('/', tweetController.createTweet);
+// 글번호에 대한 트윗 가져오기
+// GET
+// http://localhost:8080/tweets/:id
+router.get('/:id', tweetController.getTweet);
 
 
-            // 트윗 수정하기
-            // PUT
-            // http://localhost:8080/tweets/:id
-            // id, username, text
-            // json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
-            router.put('/:id', tweetController.updateTweet);
+// 트윗하기
+// POST
+// http://localhost:8080/tweets
+// name, username, text
+// json 형태로 입력 후 추가된 데이터까지 모두 json으로 출력
+router.post('/', validateTweet, tweetController.createTweet); //추가
 
 
-            // 트윗 삭제하기
-            // DELETE
-            // http://localhost:8080/tweets/:id
-            router.delete('/:id', tweetController.deleteTweet);
+// 트윗 수정하기
+// PUT
+// http://localhost:8080/tweets/:id
+// id, username, text
+// json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
+router.put('/:id', validateTweet, tweetController.createTweet); //추가
 
 
-            export default router;
+// 트윗 삭제하기
+// DELETE
+// http://localhost:8080/tweets/:id
+router.delete('/:id', tweetController.deleteTweet);
+
+
+export default router;
